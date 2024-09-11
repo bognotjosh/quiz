36b3628d51d74df2578b52d86700b33fa8924aa2
Josh <bognotjosh@gmail.com>
2024-09-12 01:59:49 +0800

    Add division method

--- calculator.java
+++ calculator.java
@@ -6,8 +6,16 @@ public class calculator {
     public static int subtract(int a, int b) {
         return a - b;
     }
+
     public static int multiply(int a, int b) {
         return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
     }
 
     public static void main(String[] args) {
@@ -15,6 +23,7 @@ public class calculator {
         System.out.println("Addition: " + add(5, 3)); 
         System.out.println("Subtraction: " + subtract(5, 3));
         System.out.println("Multiplication: " + multiply(5, 3));
+        System.out.println("Division: " + divide(6, 3));
      }
  }
  
